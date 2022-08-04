7dc6378addb801c4e420339e6380a45d3440445e
pachikuriii <stakahashi0728@gmail.com>
2022-08-04 09:56:07 +0900

    リファクタリング完了

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🗞 the-birthday-times 🗞
+![image](https://user-images.githubusercontent.com/58751858/182638789-a1eef5f4-5f01-4ec8-95a5-3c3c283fdc39.png)
+Shows articles published on your birthday from The New York Times.
+ 
+# Installation
+```
+$ npm i -g the-birthday-times
+```
+ 
+# Usage
+## 1. Set your API key
+```
+$ export NYTIMES_KEY='YOUR-API-KEY'
+```
+⚡️ You need The New York Times Developer Network account to create API key. More info is [here](https://developer.nytimes.com/get-started)
+
+
+## 2. Run `$ the-birthday-times`
+
+
+## 3. Answer a question
+![image](https://user-images.githubusercontent.com/58751858/182617646-2a78d057-eb54-495c-8bc9-0fe37d03a8c5.png)
+
+
+## 4. The result appears.
+![image](https://user-images.githubusercontent.com/58751858/182620539-38de904d-0921-41dc-9a63-da8495c6c7a4.png)
--- the-birthday-times.js
+++ the-birthday-times.js
@@ -1,1 +1,112 @@
-test
+const needle = require('needle')
+const dayjs = require('dayjs')
+const prompts = require('prompts')
+const color = require('ansi-colors')
+
+function main () {
+  try {
+    if (!process.env.NYTIMES_KEY) {
+      throw new Error(color.red("Please set your API key on your operating system.\nTo set environment variables on macOS or Linux, run the export command from the terminal: export NYTIMES_KEY='YOUR-API-KEY'\n"))
+    }
+    showFirstMessage()
+    getBirthdayNews()
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+function showFirstMessage () {
+  console.log('\n' +
+':￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣: \n' +
+':                                                                      :')
+  console.log(color.bold(':              🗞   T h e   B i r t h d a y   T i m e s 🗞               :'))
+  console.log(
+    ':                                                                      : \n' +
+':                                                                      : \n' +
+' ￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣ \n\n')
+}
+
+async function getBirthdayNews () {
+  const question = [
+    {
+      type: 'date',
+      name: 'birthday',
+      message: '🎂 Your Birthday?',
+      mask: ('YYYY-MM-DD'),
+      validate: date => date > dayjs()
+        ? 'Not in the future'
+        : true &&
+      date < dayjs('1981/1/1')
+          ? 'Not before 1981'
+          : true
+    }
+  ]
+  const response = await prompts(question)
+  const birthday = dayjs(response.birthday).add(1, 'month')
+  setTimeout(countDown)
+  displayNews(birthday)
+}
+
+function countDown () {
+  process.stdout.write('\n\n\n                 .')
+  blockTime(700)
+  process.stdout.write('.')
+  blockTime(700)
+  process.stdout.write('.')
+  blockTime(700)
+  process.stdout.write('.')
+  blockTime(700)
+  process.stdout.write('.')
+  blockTime(700)
+  console.log(' Now going back to your birthday' + '\n\n\n')
+  blockTime(700)
+}
+
+function blockTime (timeout) {
+  const startTime = Date.now()
+  while (true) {
+    const diffTime = Date.now() - startTime
+    if (diffTime >= timeout) {
+      return
+    }
+  }
+}
+
+async function displayNews (birthday) {
+  const response = await getRequest(birthday)
+  const newsIndexNum = getNewsIndexNum()
+
+  try {
+    for (let index = 0; index < newsIndexNum.length; index++) {
+      console.log('🔎 ' + color.bold.green.underline(response.body.response.docs[newsIndexNum[index]].headline.main + '\n'))
+      console.log(response.body.response.docs[newsIndexNum[index]].lead_paragraph + '\n\n\n')
+    }
+  } catch (error) {
+    console.log('🔎 ' + color.bold.red.underline('Sorry...the article cannot be found.\n\n\n\n'))
+  }
+}
+
+async function getRequest (birthday) {
+  const birthYear = (birthday.$y)
+  const birthMonth = (birthday.$M)
+  const birthDate = (birthday.$D)
+  const response = await needle('get', `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=pub_date:(${birthYear}-${birthMonth}-${birthDate})&api-key=${process.env.NYTIMES_KEY}`)
+  if (response) {
+    return response
+  } else {
+    throw new Error('The API key is incorrect. Please check it again.'
+    )
+  }
+}
+
+function getNewsIndexNum () {
+  const newsIndexNum = []
+  while (newsIndexNum.length <= 2) {
+    const index = Math.floor(Math.random() * (11 - 1) + 1)
+    if (newsIndexNum[0] !== index && newsIndexNum[newsIndexNum.length - 1] !== index) {
+      newsIndexNum.push(index)
+    }
+  }
+  return newsIndexNum
+}
+main()
